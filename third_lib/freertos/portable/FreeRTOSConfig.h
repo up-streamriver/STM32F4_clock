d389bddcd1773b5ece5f62ebc42d23c0ebae0639
up-streamriver <940259893@qq.com>
2026-02-01 12:39:27 +0800

    finish freertos

--- third_lib/freertos/portable/FreeRTOSConfig.h
+++ third_lib/freertos/portable/FreeRTOSConfig.h
@@ -84,9 +84,9 @@ void vAssertCalled(const char *file, int line);
 #define INCLUDE_uxTaskGetStackHighWaterMark2    1
 #define INCLUDE_xTaskGetIdleTaskHandle          1
 #define INCLUDE_eTaskGetState                   1
-#define INCLUDE_xTimerPendFunctionCall          0
+#define INCLUDE_xTimerPendFunctionCall          1
 #define INCLUDE_xTaskAbortDelay                 0
-#define INCLUDE_xTaskGetHandle                  1
+#define INCLUDE_xTaskGetHandle                  0
 #define INCLUDE_xTaskResumeFromISR              1
 
 /* A header file that defines trace macro can be included here. */
